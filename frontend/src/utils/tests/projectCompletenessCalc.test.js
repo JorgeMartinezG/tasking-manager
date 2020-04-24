585ad44fdf7bcbb9901f0cc837f76b3ec62e048d
Wille Marcel <wille.yyz@gmail.com>
2020-04-23 22:55:15 -0300

    update completeness computation on frontend

--- frontend/src/utils/tests/projectCompletenessCalc.test.js
+++ frontend/src/utils/tests/projectCompletenessCalc.test.js
@@ -3,6 +3,6 @@ import { tasksGeojson } from './snippets/tasksGeometry';
 
 it('computeCompleteness', () => {
   const project = computeCompleteness(tasksGeojson);
-  expect(project.percentMapped).toBe(28);
-  expect(project.percentValidated).toBe(14);
+  expect(project.percentMapped).toBe(25);
+  expect(project.percentValidated).toBe(12);
 });
